35ebd036403f180892166415276412a13a58b765
bishk <abhishekdpseok@gmail.com>
2016-12-10 17:04:28 -0500

    adjustable beta

--- datagen.py
+++ datagen.py
@@ -1,47 +1,47 @@
 import random
-import numpy 
+import numpy
 from operator import add
 import csv
 from sklearn.decomposition import PCA
 from numpy import genfromtxt
 
-
-betas = [[[0, 0, 0, 0],
+beta = 5
+betas = beta *numpy.array([[[0, 0, 0, 0],
           [1, 0, 0, 0],
           [1, 0, 0, 0],
           [0, 0, 0, 0]],
-        [[.6, 0, .6, 1],
-         [0, 0, 0, 0],
-         [0, 0, 0, 0],
-         [.6, 0, .6, 1]],
-        [[.3, 2, 0, .8],
-         [0, 0, 0, 0],
-         [0, 0, 0, 0],
-         [.3, 2, 0, .8]],
-        [[0.2, 1, 0, 0.8],
-         [0, 0, 0, 0],
-         [0, 0, 0, 0],
-         [0.2, 1, 0, 0.8]],
-        [[0, 0, 0, 0],
-         [0.6, 0, 1, 0],
-         [0.6, 0, 1, 0],
-         [0, 0, 0, 0]],
-        [[0.2, 0.2, 1.2, 0.8],
-         [0.6, 0.6, 0.2, 0],
-         [0.6, 0.6, 0.2, 0],
-         [0.2, 0.2, 1.2, 0.8]],
-        [[0.2, 0, 1.2, 0.8],
-         [0.4, 0, 1, 1],
-         [0, 0, 0, 0],
-         [0.2, 0, 1, 0.8]],
-        [[0, 0, 0, 0],
-         [0.8, 0, 1, 0.2],
-         [0.8, 0, 1, 0.2],
-         [0, 0, 0, 0]],
-        [[0.6, 0, 0, 1],
-         [0, 0, 0, 0],
-         [0, 0, 0, 0],
-         [0, 0, 0, 0]]]
+         [[.6, 0, .6, 1],
+          [0, 0, 0, 0],
+          [0, 0, 0, 0],
+          [.6, 0, .6, 1]],
+         [[.3, 2, 0, .8],
+          [0, 0, 0, 0],
+          [0, 0, 0, 0],
+          [.3, 2, 0, .8]],
+         [[0.2, 1, 0, 0.8],
+          [0, 0, 0, 0],
+          [0, 0, 0, 0],
+          [0.2, 1, 0, 0.8]],
+         [[0, 0, 0, 0],
+          [0.6, 0, 1, 0],
+          [0.6, 0, 1, 0],
+          [0, 0, 0, 0]],
+         [[0.2, 0.2, 1.2, 0.8],
+          [0.6, 0.6, 0.2, 0],
+          [0.6, 0.6, 0.2, 0],
+          [0.2, 0.2, 1.2, 0.8]],
+         [[0.2, 0, 1.2, 0.8],
+          [0.4, 0, 1, 1],
+          [0, 0, 0, 0],
+          [0.2, 0, 1, 0.8]],
+         [[0, 0, 0, 0],
+          [0.8, 0, 1, 0.2],
+          [0.8, 0, 1, 0.2],
+          [0, 0, 0, 0]],
+         [[0.6, 0, 0, 1],
+          [0, 0, 0, 0],
+          [0, 0, 0, 0],
+          [0, 0, 0, 0]]])
 
 sd = 0.1
 def checker(i,n):
@@ -49,7 +49,7 @@ def checker(i,n):
         return 1
     else:
         return 0
-    
+
 def genclass(n):
     classes = ['1000', '1100', '1110', '1101', '1010', '1001', '1011', '1111', '0100', '0110', '0101', '0111', '0010', '0011', '0001', '0000']
     people = []
@@ -63,7 +63,7 @@ def function(n, betas):
     final  = []
     mylist = genclass(n)
     a = 0
-
+    
     # create numpy array to store actual classes
     # array with shape ([class], 4)
     classes_actual = numpy.zeros(shape=(n, 4))
@@ -73,49 +73,49 @@ def function(n, betas):
         for j in range(9):
             values = [random.gauss(0, sd), random.gauss(0, sd),
                       random.gauss(0, sd), random.gauss(0, sd)]
-            for c in range(4):
-                newvalues = [checker(i,c)*random.gauss(betas[j][0][c], sd), 
-                             checker(i,c)*random.gauss(betas[j][1][c], sd), 
-                             checker(i,c)*random.gauss(betas[j][2][c], sd), 
-                             checker(i,c)*random.gauss(betas[j][3][c], sd) ]
-                for z in range(3):
-                    values[z] = values[z] + newvalues[z]
-            ovr = ovr + values
-        if a == 0:
-            final = ovr
+                      for c in range(4):
+                          newvalues = [checker(i,c)*random.gauss(betas[j][0][c], sd),
+                                       checker(i,c)*random.gauss(betas[j][1][c], sd),
+                                       checker(i,c)*random.gauss(betas[j][2][c], sd),
+                                       checker(i,c)*random.gauss(betas[j][3][c], sd) ]
+                              for z in range(3):
+                                  values[z] = values[z] + newvalues[z]
+                              ovr = ovr + values
+    if a == 0:
+        final = ovr
         else:
             final = numpy.column_stack((final, ovr))
 
-        # add class information to numpy array
-        for ii in range(4):
-        	classes_actual[a][ii] = checker(i, ii)
-
-        a = a+1
-        # print i, numpy.mean(ovr)
-
+# add class information to numpy array
+for ii in range(4):
+    classes_actual[a][ii] = checker(i, ii)
         
-    # transpose shape for classes for convenience 
+        a = a+1
+    # print i, numpy.mean(ovr)
+
+
+    # transpose shape for classes for convenience
     final = numpy.transpose(final)
     print final
     print final.shape
     print classes_actual
     print classes_actual.shape
 
-    with open("output_tests.csv", "wb") as f:
-        writer = csv.writer(f)
+with open("output_tests.csv", "wb") as f:
+    writer = csv.writer(f)
         writer.writerows(final)
-
-
+    
+    
     with open("output_classes.csv", "wb") as f:
         writer = csv.writer(f)
         writer.writerows(classes_actual)
 
-    
+
 function(10000, betas)
 # people = array of 10000 classes
 
 
-drive_data_path = 'output_tests.csv' 
+drive_data_path = 'output_tests.csv'
 
 class_data_path = 'output_classes.csv'
 
@@ -133,6 +133,3 @@ pca.fit(data_drive)
 print(pca.explained_variance_ratio_) 
 
 # Now use those to test against other 2000 people
-
-
-
