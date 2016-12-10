8746efd7bc38d88c28b4dd655ee856d927c11ad3
stephenslater <stephen.slater95@gmail.com>
2016-12-10 18:17:36 -0500

    Plot of features vs. % variance explained for different betas

--- datagen.py
+++ datagen.py
@@ -8,7 +8,8 @@ import matplotlib.pyplot as plt
 
 
 
-beta_vals = [0.05, 0.15, 0.25, 0.35, 0.50, 0.75, 1.00, 5.00]
+beta_vals = [0.01, 0.05, 0.15, 0.25, 0.50, 0.75, 1.00]
+
 for x in beta_vals:
     betas = x * numpy.array([[[0, 0, 0, 0],
                               [1, 0, 0, 0],
@@ -46,25 +47,25 @@ for x in beta_vals:
                               [0, 0, 0, 0],
                               [0, 0, 0, 0],
                               [0, 0, 0, 0]]])
-                              
-                              sd = 0.1
-                              def checker(i,n):
-                                  if i[n] == '1':
-                                      return 1
-                                          else:
-                                              return 0
+      
+    sd = 0.1
+    def checker(i,n):
+        if i[n] == '1':
+            return 1
+        else:
+            return 0
 
-def genclass(n):
-    classes = ['1000', '1100', '1110', '1101', '1010', '1001', '1011', '1111', '0100', '0110', '0101', '0111', '0010', '0011', '0001', '0000']
+    def genclass(n):
+        classes = ['1000', '1100', '1110', '1101', '1010', '1001', '1011', '1111', '0100', '0110', '0101', '0111', '0010', '0011', '0001', '0000']
         people = []
         for i in range(n):
             people.append(random.choice(classes))
-    return people
+        return people
 
 
 
-def function(n, betas):
-    final  = []
+    def function(n, betas):
+        final  = []
         mylist = genclass(n)
         a = 0
         
@@ -77,52 +78,39 @@ def function(n, betas):
             for j in range(9):
                 values = [random.gauss(0, sd), random.gauss(0, sd),
                           random.gauss(0, sd), random.gauss(0, sd)]
-                          for c in range(4):
-                              newvalues = [checker(i,c)*random.gauss(betas[j][0][c], sd),
-                                           checker(i,c)*random.gauss(betas[j][1][c], sd),
-                                           checker(i,c)*random.gauss(betas[j][2][c], sd),
-                                           checker(i,c)*random.gauss(betas[j][3][c], sd) ]
-                                  for z in range(3):
-                                      values[z] = values[z] + newvalues[z]
-                                  ovr = ovr + values
-        if a == 0:
-            final = ovr
+                for c in range(4):
+                    newvalues = [checker(i,c)*random.gauss(betas[j][0][c], sd),
+                               checker(i,c)*random.gauss(betas[j][1][c], sd),
+                               checker(i,c)*random.gauss(betas[j][2][c], sd),
+                               checker(i,c)*random.gauss(betas[j][3][c], sd) ]
+                for z in range(3):
+                    values[z] = values[z] + newvalues[z]
+                ovr = ovr + values
+            if a == 0:
+                final = ovr
             else:
                 final = numpy.column_stack((final, ovr))
 
-        # add class information to numpy array
-        for ii in range(4):
-            classes_actual[a][ii] = checker(i, ii)
-            
+            # add class information to numpy array
+            for ii in range(4):
+                classes_actual[a][ii] = checker(i, ii)
+                
             a = a+1
         # print i, numpy.mean(ovr)
 
-
-# transpose shape for classes for convenience
-final = numpy.transpose(final)
-    
-    with open("output_tests.csv", "wb") as f:
-        writer = csv.writer(f)
-            writer.writerows(final)
+        # transpose shape for classes for convenience
+        final = numpy.transpose(final)
+        return final, classes_actual
         
-        
-        with open("output_classes.csv", "wb") as f:
-            writer = csv.writer(f)
-            writer.writerows(classes_actual)
-    
+    data_actual, classes_actual = function(10000, betas)
+    print data_actual
+    print classes_actual
 
-function(10000, betas)
-    # people = array of 10000 classes
-    
-    drive_data_path = 'output_tests.csv' 
-    class_data_path = 'output_classes.csv'
-    
-    data_drive = genfromtxt(drive_data_path, delimiter=',')
-    data_classes = genfromtxt(class_data_path, delimiter=',')
-    
+    print data_actual.shape
+
     pca = PCA(n_components=36)
     
-    pca.fit(data_drive)
+    pca.fit(data_actual)
     
     # Do something with data_classes
     
@@ -142,7 +130,7 @@ function(10000, betas)
     plt.plot(array, array2)
     plt.xlabel('N features')
     plt.ylabel('% variance explained')
-    plt.show()
+plt.show()
 
 
 # Now use those to test against other 2000 people
